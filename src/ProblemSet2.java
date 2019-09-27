855b3d3b012f3564209968615e68a3ef52705cce
Naya-Jevan Goodman <ngoodman@ucvts.org>
2019-09-27 13:35:27 -0400

    intial commit

--- src/ProblemSet2.java
+++ src/ProblemSet2.java
@@ -1,4 +1,4 @@
-/**
+        /**
  * Problem Set 2.
  * 
  * It's time to put your skills to the test. This problem set focuses on prompting
@@ -27,17 +27,72 @@ public class ProblemSet2 {
          * Prompt the user to enter the following information (in order): first name, last
          * name, grade, age, and hometown.
          */
-
+		 
+		System.out.print("\nEnter your first name: \n");
+		String firstName = in.nextLine();
+		System.out.print("\nEnter your last name: \n");
+		String lastName = in.nextLine();
+		System.out.print("\nEnter your grade: \n");
+		int grade = in.nextInt();
+		System.out.print("\nEnter your age: \n");
+		int age = in.nextInt();
+		System.out.print("\nEnter your hometown: \n");
+		String homeTown = in.nextLine();
+		//asks first name, last name, grade, age, and hometown (NAYA)
+		
         
-        
+		System.out.println("\nNAME\t:”  + “ ” + firstName + " " + lastName"\n");
+        System.out.println("\nGRADE\t:" + “ ” + grade + "\n");
+		System.out.println("\nAGE\t:" + “ ” + age + "\n");
+		System.out.println("\HOMETOWN\t:" + “ ” + homeTown + "\n");
+		/*prints first name, last name, grade, age, and hometown
+		 *based on the response of the code above (NAYA)
+		 */
+		
         /*
          * Exercise 2.
          * 
-         * Given a dollar amount in the range [0.00, 1.00], print the number of dollar
+         * Given a dollar amount in the range [0.00, 1.00], print the number of dollars
          * bills, quarters, dimes, nickels, and pennies needed to produce this amount.
          */
-
-
+		 
+		double dollar = 100;
+		double quarter = 25;
+		double dime = 10;
+		double nickel = 5;
+		double penny = 1;
+		// change amount (NAYA)
+		
+		System.out.print("\nEnter a dollar amount: ");
+		double.amount = in.nextDouble();
+		System.out.println("\n ");
+		//asks the dollar amount (NAYA)
+		
+		amount = amount * 100;
+		double amountOfDollars = Math.floor(amount/dollar);
+		amount = amount % dollar;
+		double amountOfQuarters = Math.floor(amount/quarter);
+		amount = amount % quarter;
+		double amountOfDimes = Math.floor(amount/dime);
+		amount = amount % dime;
+		double amountOfNickels = Math.floor(amount/nickel);
+		amount = amount % nickel;
+		double amountOfPennies = Math.floor(amount/penny);
+		amount = amount % penny;
+		
+		/*calculates the number of dollars needed and number of coins needed
+		 *based on the remainder (NAYA)
+		 */
+		
+		System.out.printf("/nDOLLARS\t\t: %.0f", amountOfDollars);
+		System.out.printf("/nQUARTERS\t\t: %.0f", amountOfQuarters);
+		System.out.printf("/nDIMES\t\t: %.0f", amountOfDimes);
+		System.out.printf("/nNICKELS\t\t: %.0f", amountOfNickels);
+		System.out.printf("/nPENNIES\t\t: %.0f", amountOfPennies);
+		
+		/*prints bills, quarters, dimes, nickels, and pennies
+		 *based on the response of the code above (NAYA)
+		 */
 
         /*
          * Exercise 3.
@@ -45,7 +100,44 @@ public class ProblemSet2 {
          * Given a dollar amount in the range [0.00, 20.00], print the smallest number of
          * bills and coins needed to produce this amount.
          */
+		 
+		 double tenDollars = 1000;
+		 double fiveDollars = 500;
+		 // limit
+		 
+		 System.out.print(“\nEnter a dollar amount: ”);
+		 double amount2 = in.nextDouble();
+         System.out.println("\n ");
+         amount2 = amount2 * 100;
+		 //asks the dollar amount (NAYA)
 
+         double TEN = Math.floor(amount2/ten);
+         amount2 = amount2 % ten;
+         double FIVE = Math.floor(amount2/five);
+         amount2 = amount2 % five;
+         double amountOfDollars2 = Math.floor(amount2/dollar);
+         amount2 = amount2%dollar;
+         double bills = TEN + FIVE + amountOfDollars2;
+		 /*converts all change to bills
+		 *adds all of the bills up
+		 */
+
+         double amountOfQuarters2 = Math.floor(amount2/quarter);
+         amount2 = amount2 % quarter;
+         double amountOfDimes2 = Math.floor(amount2/dime);
+         amount2 = amount2 % dime;
+         double amountOfNickels2 = Math.floor(amount2/nickel);
+         amount2 = amount2 % nickel;
+         double amountOfPennies2 = amount2/penny;
+         amount2 = amount2 % penny;
+		 double coins = amountOfQuarters2+amountOfDimes2+amountOfNickels2+amountOfPennies2; 	
+		/*converts all change to the remainder
+		 *adds all of the change up
+		 */
+
+         System.out.printf("BILLS : %.0f\n", bills);
+         System.out.printf("COINS : %.0f\n", coins);
+		//prints the amount of coins and bills
 
 
         /*
